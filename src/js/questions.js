1ba3ae6a1d36ea8301574c28584e6832ab78cf14
Haranmes <soliman.harames15@gmail.com>
2022-06-30 14:39:02 +0200

    New Quizformat

---
+++ src/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Wo traffen sich Magarete und Faust zum ersten Mal?",
+    answer: "Auf einer Straße",
+    options: [
+      "In ein Restaurant",
+      "Bei Gretchen Zuhause",
+      "Auf einer Straße",
+      "In einem Garten"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Wie wurde im späterem Verlauf des Buches Magerete denn genannt?",
+    answer: "Gretchen",
+    options: [
+      "Gretchen",
+      "Susanne",
+      "Martha",
+      "Lieschen"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Wann wurde Magarete zum ersten Mal Gretchen genannt?",
+    answer: "Szene: Gretchens Stube",
+    options: [
+      "Szene: Gartenhäuschen",
+      "Szene: Straße",
+      "Szene: Marthens Garten",
+      "Szene: Gretchens Stube"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Welche Eigenschaften passen am besten zu Gretchen?",
+    answer: "unschuldig, naiv, rein",
+    options: [
+      "Ruhig, durchdacht, kühl, abweisend ",
+      "unschuldig, naiv, rein",
+      "Energetisch, impulsiv, dynamisch ",
+      "Pessimistisch"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Wenn hatte Gretchen alles aktiv getötet?",
+    answer: "Mutter und Kind",
+    options: [
+      "Vater und Bruder",
+      "Bruder und Schwester",
+      "Schwester und Mutter",
+      "Mutter und Kind"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "",
+    answer: "Correct answer of the question is here",
+    options: [
+      "Option 1",
+      "option 2",
+      "option 3",
+      "option 4"
+    ]
+  },
+];
